7cf891163fad3dbd92578abc506a453f0739efca
Valeriy Litovchenko <valeriylprog@gmail.com>
2022-10-07 19:28:14 +0100

    Add SwiftLint

--- OfficeeApp/OfficeeApp/AppDelegate.swift
+++ OfficeeApp/OfficeeApp/AppDelegate.swift
@@ -10,11 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
+  // MARK: - Properties
+  
   var window: UIWindow?
+  
+  // MARK: - Functions
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
   }
 }
-
