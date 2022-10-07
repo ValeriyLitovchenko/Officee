26faf772c6a2a2af0cd578b1d751bc55529c188a
Valeriy Litovchenko <valeriylprog@gmail.com>
2022-10-07 19:16:16 +0100

    initial commit

---
+++ OfficeeApp/OfficeeApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  OfficeeApp
+//
+//  Created by Valeriy L on 07.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
+
