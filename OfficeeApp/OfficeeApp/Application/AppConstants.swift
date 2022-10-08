66b66c8bb5371e5a7166cc26e80353f0b591ce3b
Valeriy Litovchenko <valeriylprog@gmail.com>
2022-10-08 18:44:18 +0100

    Add AppConstants.

---
+++ OfficeeApp/OfficeeApp/Application/AppConstants.swift
@@ -0,0 +1,13 @@
+//
+//  AppConstants.swift
+//  OfficeeApp
+//
+//  Created by Valeriy L on 08.10.2022.
+//
+
+import Foundation
+
+enum AppConstants {
+  static let apiBaseURLString = "https://61e947967bc0550017bc61bf.mockapi.io/api/v1"
+  static let storageName = "officee-storage.sqlite"
+}
