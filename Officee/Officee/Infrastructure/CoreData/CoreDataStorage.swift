a2266b8a0207e885b9b6a952fef7fc2e407fc3e0
Valeriy Litovchenko <valeriylprog@gmail.com>
2022-10-08 11:05:50 +0100

    Add CoreDataStorage.

---
+++ Officee/Officee/Infrastructure/CoreData/CoreDataStorage.swift
@@ -0,0 +1,68 @@
+//
+//  CoreDataStorage.swift
+//  Officee
+//
+//  Created by Valeriy L on 08.10.2022.
+//
+
+import CoreData
+
+public final class CoreDataStorage {
+  
+  // MARK: - Properties
+  
+  private static let modelName = "OfficeeStore"
+  private static let model: NSManagedObjectModel? = {
+    Bundle(for: CoreDataStorage.self)
+      .url(forResource: modelName, withExtension: "momd")
+      .flatMap(NSManagedObjectModel.init(contentsOf:))
+  }()
+  
+  private let persistentContainer: NSPersistentContainer
+  private let context: NSManagedObjectContext
+  
+  enum StoreError: Error {
+    case modelNotFound
+  }
+  
+  // MARK: - Constructor
+  
+  public init(storeURL: URL) throws {
+    guard let model = CoreDataStorage.model else {
+      throw StoreError.modelNotFound
+    }
+    
+    let description = NSPersistentStoreDescription(url: storeURL)
+    let container = NSPersistentContainer(name: CoreDataStorage.modelName, managedObjectModel: model)
+    container.persistentStoreDescriptions = [description]
+    persistentContainer = NSPersistentContainer(name: CoreDataStorage.modelName)
+    
+    var error: Swift.Error?
+    persistentContainer.loadPersistentStores { error = $1 }
+    try error.map { throw $0 }
+    
+    context = persistentContainer.newBackgroundContext()
+  }
+  
+  deinit {
+    cleanUpReferencesToPersistentStores()
+  }
+  
+  // MARK: - Functions
+  
+  public func performSync<Result>(_ action: (NSManagedObjectContext) -> Swift.Result<Result, Error>) throws -> Result {
+    let context = self.context
+    var result: Swift.Result<Result, Error>!
+    context.performAndWait {
+      result = action(context)
+    }
+    return try result.get()
+  }
+  
+  private func cleanUpReferencesToPersistentStores() {
+    context.performAndWait {
+      let coordinator = self.persistentContainer.persistentStoreCoordinator
+      try? coordinator.persistentStores.forEach(coordinator.remove)
+    }
+  }
+}
