1a9cfe540c6da97339143d6c2b76372e68abbb29
Valeriy Litovchenko <valeriylprog@gmail.com>
2022-10-28 10:55:23 +0100

    Add OfficeeAppTests target.

---
+++ OfficeeApp/OfficeeAppTests/OfficeeAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  OfficeeAppTests.swift
+//  OfficeeAppTests
+//
+//  Created by Valeriy L on 28.10.2022.
+//
+
+import XCTest
+
+final class OfficeeAppTests: XCTestCase {
+  func test_example() throws {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Any test you write for XCTest can be annotated as throws and async.
+    // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+    // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+  }
+}
